8093031310f0d1a92b93c2dbd0c2ef5e654d2e05
Mohib <92154638+developerMohib@users.noreply.github.com>
2023-06-08 23:57:39 +0600

    only js code deploy. area nirnoy

---
+++ app.js
@@ -0,0 +1,27 @@
+// even odd number selection problem from codefores
+
+let anyNumber = parseInt(prompt(`Enter your Number : `));
+
+if(anyNumber % 2 == 0){
+    console.log(`This Number ( ${anyNumber} ) is Even `)
+}else{
+    console.log(`This Number ( ${anyNumber} ) is Odd`)
+}
+
+// area calculation
+// ayoto ketro 
+
+let base = parseInt( 4 );
+let height = parseInt( 8 );
+
+let ayotoKetroFol = base * height ;
+console.log(`ayoto ketrer ketroFol = ` + ayotoKetroFol);
+
+// trivuj 
+
+let vumi = parseInt( 4 );
+let trHeight = parseInt( 3 );
+
+let trivujKetroFol = vumi * ( 1 / 2 * trHeight);
+console.log(`Trivuj KetroFol = ` + trivujKetroFol)
+
